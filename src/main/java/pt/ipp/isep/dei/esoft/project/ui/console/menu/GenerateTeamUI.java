dae364e2b516a6894db2480d3e0141e87f3afb22
fprimavera9 <157392510+fprimavera9@users.noreply.github.com>
2024-04-30 10:44:34 +0100

    Some updates to the HRMUI, GenerateTeamUI and their controllers.

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/GenerateTeamUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/GenerateTeamUI.java
@@ -1,8 +1,9 @@
 package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
 import pt.ipp.isep.dei.esoft.project.application.controller.authorization.GenerateTeamController;
+import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
-public class GenerateTeamUI {
+public class GenerateTeamUI implements Runnable {
 
     private static GenerateTeamController ctrl;
 
@@ -11,7 +12,9 @@ public class GenerateTeamUI {
     }
 
     public void run() {
-        
+        int min = Utils.readIntegerFromConsole("Min team size: ");
+        int max = Utils.readIntegerFromConsole("Max team size: ");
+        // need the skills created
     }
 
 }
--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/HRMUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/HRMUI.java
@@ -2,6 +2,7 @@ package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 
 import pt.ipp.isep.dei.esoft.project.ui.console.CreateTaskUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.ShowTextUI;
+import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,26 @@ public class HRMUI {
 
     public void run() {
         List<MenuItem> options = new ArrayList<MenuItem>();
-        // options.add(new MenuItem("Generate Team", new GenerateTeamUI()));
+        options.add(new MenuItem("Generate Team", new GenerateTeamUI()));
+
+        int option;
+
+        do {
+
+            option = Utils.showAndSelectIndex(options, "\n\n--- HRM MENU -------------------------");
+
+            if ((option >= 0) && (option < options.size())) {
+
+                options.get(option).run();
+
+            }
+
+        } while (option != -1);
 
     }
+
+
+
+
+
 }
