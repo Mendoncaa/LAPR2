c52e354db6125d6bb4223470bfa35134cf190130
fprimavera9 <157392510+fprimavera9@users.noreply.github.com>
2024-05-11 13:22:41 +0100

    Some changes to the GenerateTeamUI

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/GenerateTeamUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/menu/GenerateTeamUI.java
@@ -3,6 +3,9 @@ package pt.ipp.isep.dei.esoft.project.ui.console.menu;
 import pt.ipp.isep.dei.esoft.project.application.controller.authorization.GenerateTeamController;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateTeamUI implements Runnable {
 
     private static GenerateTeamController ctrl;
@@ -14,7 +17,30 @@ public class GenerateTeamUI implements Runnable {
     public void run() {
         int min = Utils.readIntegerFromConsole("Min team size: ");
         int max = Utils.readIntegerFromConsole("Max team size: ");
-        // need the skills created
+        int skills = Utils.readIntegerFromConsole("Num skills: ");
+
+        for (int i = 0; i < skills; i++) {
+
+        }
+
+
+        List<String> approval = new ArrayList<String>();
+        approval.add("Yes");
+        approval.add("No");
+
+        int option;
+
+        do {
+
+            option = Utils.showAndSelectIndex(approval, "\n\n--- HRM MENU -------------------------");
+
+            if (option == 0) {
+                System.out.println("Yes test");
+            }
+
+            System.out.println("No test");
+
+        } while (option != -1);
     }
 
 }
